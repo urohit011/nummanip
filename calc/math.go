2041d056883035a2fe22da9dc958de57bdc94a4b
Rohit Upadhyay <urohit011@gmail.com>
2020-08-10 22:23:51 +0530

    new release version 2.0.0

--- calc/math.go
+++ calc/math.go
@@ -1,8 +1,16 @@
 package calc
 
+import (
+	"errors"
+)
+
 // Add is sued to add two numbers
 func Add(numbers ...int) int {
 	sum := 0
+
+	if len(numbers) < 2 {
+		return errors.New("Provide more than 2 arguments"), sum
+	}
 
 	for _, num := range numbers {
 		sum += num
