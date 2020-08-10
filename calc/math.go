9574ac2d3ac35845ec9dfbca35b0af67b9276315
Rohit Upadhyay <urohit011@gmail.com>
2020-08-10 22:00:20 +0530

    Add is a variadic function now

--- calc/math.go
+++ calc/math.go
@@ -1,6 +1,11 @@
 package calc
 
 // Add is sued to add two numbers
-func Add(i, j int) int {
-	return i + j
+func Add(numbers ...int) int {
+	sum := 0
+
+	for _, num := range numbers {
+		sum += num
+	}
+	return sum
 }
